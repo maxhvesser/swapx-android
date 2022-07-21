f5d569b6a5ca19d9dc383de57a20c68aee991fc1
Maximilian Hvesser-Lewis <maxhvesser@gmail.com>
2022-07-21 20:52:24 +0200

    Added currency flag assets

--- app/src/main/java/uk/mhl/swapx/ui/view/CurrencyRow.kt
+++ app/src/main/java/uk/mhl/swapx/ui/view/CurrencyRow.kt
@@ -3,10 +3,7 @@ package uk.mhl.swapx.ui.view
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -54,6 +51,7 @@ private fun BaseCurrencyRow(
         horizontalArrangement = Arrangement.spacedBy(12.dp)
     ) {
         Image(
+            modifier = Modifier.size(24.dp),
             painter = icon,
             contentDescription = null
         )
@@ -101,7 +99,7 @@ fun CurrencyRow(
 fun Preview_CurrencyRow() {
     SwapTheme {
         CurrencyRow(
-            currencyIcon = painterResource(R.drawable.ic_chevron_right),
+            currencyIcon = painterResource(R.drawable.ic_cad),
             currency = "Canadian Dollars",
             currencyCode = "CAD",
             selected = true,
