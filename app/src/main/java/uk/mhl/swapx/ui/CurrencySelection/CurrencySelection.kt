3c4a54da0238675f88eaf47b94d9eac6e289554f
Maximilian Hvesser-Lewis <maxhvesser@gmail.com>
2022-07-22 11:32:30 +0200

    Added basic list of currencies to CurrencySelection screen

--- app/src/main/java/uk/mhl/swapx/ui/CurrencySelection/CurrencySelection.kt
+++ app/src/main/java/uk/mhl/swapx/ui/CurrencySelection/CurrencySelection.kt
@@ -1,14 +1,22 @@
 package uk.mhl.swapx.ui.CurrencySelection
 
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Scaffold
 import androidx.compose.material3.Icon
 import androidx.compose.material3.SmallTopAppBar
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import uk.mhl.swapx.R
+import uk.mhl.swapx.data.local.CurrencyListProvider
 import uk.mhl.swapx.ui.theme.SwapTheme
+import uk.mhl.swapx.ui.view.CurrencyRow
 
 // region Entry
 
@@ -31,7 +39,8 @@ private fun Content() {
                 },
                 navigationIcon = {
                     Icon(
-                        painter = painterResource(R.drawable.ic_swap),
+                        modifier = Modifier.padding(12.dp, 0.dp),
+                        painter = painterResource(R.drawable.ic_arrow_back),
                         contentDescription = null
                     )
                 }
@@ -40,7 +49,26 @@ private fun Content() {
     ) {
         val padding = it
 
+        CurrencyList()
+    }
+}
 
+@Composable
+private fun CurrencyList() {
+    val currencies = CurrencyListProvider().currencies()
+    
+    LazyColumn(
+        contentPadding = PaddingValues(12.dp)
+    ) {
+        items(currencies) { currency ->
+            CurrencyRow(
+                currency = currency.name,
+                currencyCode = currency.code,
+                currencyIcon = painterResource(currency.iconAsset),
+                selected = currency.code == "CAD",
+                onRowClicked = { }
+            )
+        }
     }
 }
 
