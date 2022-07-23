20dea0d615646796a85134be9a26096207c0ab77
Maximilian Hvesser-Lewis <maxhvesser@gmail.com>
2022-07-23 11:47:02 +0200

    Added AppModule class with networking implementation

---
+++ app/src/main/java/uk/mhl/swapx/di/AppModule.kt
@@ -0,0 +1,47 @@
+package uk.mhl.swapx.di
+
+import android.content.Context
+import com.google.gson.Gson
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import uk.mhl.swapx.data.remote.ExchangeApi
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    // region Network
+
+    @Singleton
+    @Provides
+    fun provideGson(): Gson = Gson()
+
+    @Singleton
+    @Provides
+    fun provideBaseUrl(): String = ExchangeApi.BaseUrl
+
+    @Singleton
+    @Provides
+    fun provideOkHttpClient(@ApplicationContext context: Context): OkHttpClient = OkHttpClient
+        .Builder()
+        .build()
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(okHttpClient: OkHttpClient, baseUrl: String): Retrofit = Retrofit
+        .Builder()
+        .addConverterFactory(GsonConverterFactory.create())
+        .baseUrl(baseUrl)
+        .client(okHttpClient)
+        .build()
+
+    // endregion
+
+}
