5168a5996769fa731a04d4ee168956ad3a8f61c6
Maximilian Hvesser-Lewis <maxhvesser@gmail.com>
2022-07-26 09:45:07 +0200

    Added progress indicator to splash screen

--- app/src/main/java/uk/mhl/swapx/ui/splash/Splash.kt
+++ app/src/main/java/uk/mhl/swapx/ui/splash/Splash.kt
@@ -1,9 +1,15 @@
 package uk.mhl.swapx.ui.splash
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import uk.mhl.swapx.ui.theme.SwapTheme
 
@@ -31,7 +37,14 @@ fun Splash(
 
 @Composable
 private fun Content() {
-
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        CircularProgressIndicator(
+            color = MaterialTheme.colorScheme.primary
+        )
+    }
 }
 
 // endregion
@@ -42,7 +55,7 @@ private fun Content() {
 @Composable
 fun Preview_Content() {
     SwapTheme {
-
+        Content()
     }
 }
 
