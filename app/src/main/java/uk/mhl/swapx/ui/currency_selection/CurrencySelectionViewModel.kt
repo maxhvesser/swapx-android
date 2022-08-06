5b4b6b2bff6076246aa37bd1d2149525d5ca0c36
Maximilian Hvesser-Lewis <maxhvesser@gmail.com>
2022-08-06 14:15:06 +0200

    Setup basic CurrencySelectionViewModel

--- app/src/main/java/uk/mhl/swapx/ui/currency_selection/CurrencySelectionViewModel.kt
+++ app/src/main/java/uk/mhl/swapx/ui/currency_selection/CurrencySelectionViewModel.kt
@@ -1,4 +1,26 @@
 package uk.mhl.swapx.ui.currency_selection
 
-class CurrencySelectionViewModel {
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class CurrencySelectionViewModel @Inject constructor(
+
+) : ViewModel() {
+
+    // region State
+
+
+
+    // endregion
+
+    // region Initialisation
+
+    init {
+
+    }
+
+    // endregion
+
 }
