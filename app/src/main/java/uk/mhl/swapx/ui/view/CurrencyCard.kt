f51b03aa9adc383c45019de7a76d0176886aef41
Maximilian Hvesser-Lewis <maxhvesser@gmail.com>
2022-08-08 14:21:27 +0200

    Keep amount view to a single line

--- app/src/main/java/uk/mhl/swapx/ui/view/CurrencyCard.kt
+++ app/src/main/java/uk/mhl/swapx/ui/view/CurrencyCard.kt
@@ -11,6 +11,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import uk.mhl.swapx.R
@@ -39,7 +40,9 @@ private fun BaseCurrencyCard(
             modifier = Modifier.fillMaxWidth(),
             text = amount,
             style = MaterialTheme.typography.displayMedium,
-            color = MaterialTheme.colorScheme.secondary
+            color = MaterialTheme.colorScheme.secondary,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis
         )
         Row {
             Text(
