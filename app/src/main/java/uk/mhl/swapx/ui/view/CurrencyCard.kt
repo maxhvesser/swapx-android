ddfb04ad45b18aa4b065cc8e7e526cd4ffe1aa8e
Maximilian Hvesser-Lewis <maxhvesser@gmail.com>
2022-09-02 13:36:35 +0200

    Removed unused import

--- app/src/main/java/uk/mhl/swapx/ui/view/CurrencyCard.kt
+++ app/src/main/java/uk/mhl/swapx/ui/view/CurrencyCard.kt
@@ -1,6 +1,5 @@
 package uk.mhl.swapx.ui.view
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
