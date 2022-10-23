4ee2c5a42a534e2c1707d0bb7bb1d663c875af20
Maximilian Hvesser-Lewis <maxhvesser@gmail.com>
2022-10-23 15:11:45 +0200

    Upgraded datastore to 1.1.0-dev01

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -6,7 +6,7 @@ object Versions {
     const val compose = "1.3.0-rc01"
     const val composeCompiler = "1.3.2"
     const val coreKtx = "1.9.0"
-    const val datastore = "1.0.0"
+    const val datastore = "1.1.0-dev01"
     const val espresso = "3.5.0-beta01"
     const val flipper = "0.171.1"
     const val gradlePlugin = "7.3.1"
