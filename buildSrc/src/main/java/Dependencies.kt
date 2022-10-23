7a51f52d8cc6e202570597ea32a641d69f05eb48
Maximilian Hvesser-Lewis <maxhvesser@gmail.com>
2022-10-23 14:57:31 +0200

    Upgraded activity to 1.7.0-alpha01

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -2,7 +2,7 @@ package uk.swapx.build
 
 object Versions {
     const val accompanist = "0.26.5-rc"
-    const val activity = "1.6.0-alpha05"
+    const val activity = "1.7.0-alpha01"
     const val compose = "1.3.0-alpha01"
     const val composeCompiler = "1.3.2"
     const val coreKtx = "1.9.0"
