82fd827db5562aae1f022c88beacadd38b4b5589
Maximilian Hvesser-Lewis <maxhvesser@gmail.com>
2022-11-13 12:52:06 +0100

    Upgraded compose to 1.4.0-alpha02

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -3,7 +3,7 @@ package uk.swapx.build
 object Versions {
     const val accompanist = "0.26.5-rc"
     const val activity = "1.7.0-alpha02"
-    const val compose = "1.3.0-rc01"
+    const val compose = "1.4.0-alpha02"
     const val composeCompiler = "1.4.0-alpha02"
     const val coreKtx = "1.9.0"
     const val datastore = "1.1.0-dev01"
